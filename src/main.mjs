5a187e3aa8113315768fd9972d3c690d7d8896c3
chris <chrisk13fer@gmail.com>
2024-04-28 20:29:31 +0100

    writing to serial

--- src/main.mjs
+++ src/main.mjs
@@ -3,6 +3,56 @@ import { default_extensions, complete_keymap } from '@nextjournal/clojure-mode';
 import { EditorView, drawSelection, keymap } from  '@codemirror/view';
 // import {javascript} from "@codemirror/lang-javascript"
 import { syntaxHighlighting, defaultHighlightStyle, foldGutter, bracketMatching } from '@codemirror/language';
+import { extension as eval_ext, cursor_node_string, top_level_string } from '@nextjournal/clojure-mode/extensions/eval-region';
+// import {extension as eval_ext } from '@nextjournal/clojure-mode/extensions/eval-region';
+
+
+var serialport = null;
+const encoder = new TextEncoder();
+
+
+$(function() {
+  $("#btnConnect").on("click", function() {
+    console.log("uSEQ-Perform: hello")
+    console.log(navigator.serial);
+    navigator.serial.requestPort()
+    .then( (port) => {
+      port.open({baudRate:115200}).then(() => {
+        serialport = port;
+        console.log("open, writing...");
+        console.log(port.writable);
+      })
+    })
+    .catch((e) => {
+      // The user didn't select a port.
+    });
+  });
+  
+  $("#btnEval").on("click", function() {
+    console.log("eval");
+    console.log(top_level_string(state));
+  });
+});
+
+function sendTouSEQ(code) {
+  const writer = serialport.writable.getWriter();
+  console.log("writing...")
+  writer.write(encoder.encode(code)).then(() =>{
+    writer.releaseLock();
+    console.log("written")
+  });
+
+}
+
+
+navigator.serial.addEventListener('connect', e => {
+  console.log(e);
+});
+
+navigator.serial.addEventListener('disconnect', e => {
+  console.log(e);
+});
+
 
 
 import { EditorState } from  '@codemirror/state';
@@ -29,16 +79,35 @@ let theme = EditorView.theme({
 
 
 
+
+
+let evalToplevel = function (opts) {
+  console.log("eval top level")
+  let state = opts.state;
+  let code = top_level_string(state);
+  console.log(code);
+  sendTouSEQ(code);
+  return true;
+}
+
+let useqExtension = ( opts ) => {
+  return keymap.of([
+    //{key: "Alt-Enter", run: evalCell},
+                    {key: opts.modifier + "-Enter",
+                      run: evalToplevel
+                    }])}
+                
 let extensions = [keymap.of(complete_keymap),
   theme,
   foldGutter(),
   syntaxHighlighting(defaultHighlightStyle),
   drawSelection(),
   bracketMatching(),
-...default_extensions];
-
-let state = EditorState.create({doc: "... some clojure code...",
-                extensions: extensions });
+...default_extensions,
+  useqExtension({modifier: "Ctrl"})];
+                    
+let state = EditorState.create({doc: "\n\n(d2 (sqr (fast 2 bar)))",
+  extensions: extensions });
 
 let editor = new EditorView({
   state:state,
@@ -46,8 +115,5 @@ let editor = new EditorView({
   parent: document.body
 })
 
-                // let editor = new EditorView({
-                //   extensions: [basicSetup, javascript()],
-                //   parent: document.body
-                // })
-                                
+
+
