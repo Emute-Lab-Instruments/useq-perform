f6e9db4b284500b5ccc732c228b24bdd3b48d3f0
chriskiefer <chrisk13fer@gmail.com>
2024-06-28 08:20:47 +0100

    latest

--- src/main.mjs
+++ src/main.mjs
@@ -59,6 +59,8 @@ const scopedEval = (scope, script) => Function(`"use strict"; ${script}`).bind(s
 
 
 
+
+
 //keep queue of recent MIDI values  
 function uSEQ_Serial_Map(channel, value) {
 }
@@ -223,7 +225,6 @@ export function post(value) {
   if (consoleLines.length > 50) {
     consoleLines = consoleLines.slice(1);
   }
-  // $("#console").html(consoleLines.join('<br>'));
   $("#console").html(consoleLines.join(''));
   $('#console').scrollTop($('#console')[0].scrollHeight - $('#console')[0].clientHeight);
 }
@@ -290,6 +291,57 @@ let evalToplevel = function (opts, prefix="") {
   return true;
 }
 
+function upgradeCheck(versionMsg) {
+  // const verRE = /([0-9])\.([0-9])/g;
+  const verRE = /([0-9])\.([0-9])(.([0-9]))?/g;
+  const groups = verRE.exec(versionMsg);
+  console.log(groups)
+  // const groups = verRE.exec("1.0.2");
+  const moduleVersionMajor = groups[1];
+  const moduleVersionMinor = groups[2];
+  let moduleVersionPatch = 0;
+  if (groups[4]) {
+    moduleVersionPatch = groups[4];
+  }
+  post(`**Connected to uSEQ, firmware version ${versionMsg}**`);
+
+  //new release checker
+  $.ajax({
+    url: "https://api.github.com/repos/Emute-Lab-Instruments/uSEQ/releases",
+    type: "GET",
+    data: { "accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28" },
+    error: function (xhr, ajaxOptions, thrownError) {
+    }
+  }).then(function (data) {
+    //example uSEQ_1.0c_1.0.4_17072024
+    // const re = /uSEQ_(.*)_(([0-9])\.([0-9]))/g;
+    const re = /uSEQ_(.*)_(([0-9])\.([0-9])\.([0-9]))_[0-9]{8}/g;    
+    const matches = re.exec(data[0]['tag_name']);
+    const version = matches[2];
+    const ghVersionMajor = matches[3];
+    const ghVersionMinor = matches[4];
+    const ghVersionPatch = matches[5];
+    console.log(version);
+
+    //compare version
+    if (ghVersionMajor > moduleVersionMajor ||
+      (ghVersionMinor > moduleVersionMinor && ghVersionMajor >= moduleVersionMajor)
+      ||
+      (ghVersionPatch > moduleVersionPatch && ghVersionMinor >= moduleVersionMinor && ghVersionMajor >= moduleVersionMajor)
+    ) {
+      //new release available
+      post("There is a new firmware release available, click below to download");
+      post(`<a target="blank" href="${data[0]['html_url']}">${data[0]['html_url']}</a>`);
+      post("Information on how to update the module:");
+      post(`<a target="blank" href="https://emutelabinstruments.co.uk/useqinfo/useq-update/">https://emutelabinstruments.co.uk/useqinfo/useq-update/</a>`);
+    }
+
+
+  });
+
+}
+
+
 let evalNow = function (opts) {
   evalToplevel(opts, "@")
 }
@@ -309,7 +361,6 @@ let useqExtension = ( opts ) => {
                     // {key:"Alt-h", run: () => {console.log("help");}}
                   ])}
 
-                  //$("#helppanel").toggle(300)
 const updateListenerExtension = EditorView.updateListener.of((update) => {
   if (update.docChanged && config.savelocal) {
     
@@ -473,43 +524,7 @@ $(function () {
           serialReader();
           $("#btnConnect").hide(1000);
           console.log("checking version");
-          sendTouSEQ("@(useq-report-firmware-info)", (versionMsg) => {
-            //testing
-            const verRE = /([0-9])\.([0-9])/g;
-            const groups = verRE.exec(versionMsg);
-            const moduleVersionMajor = groups[1];
-            const moduleVersionMinor = groups[2];
-            post(`**Connected to uSEQ, firmware version ${versionMsg}**`);
-
-            //new release checker
-            $.ajax({
-              url: "https://api.github.com/repos/Emute-Lab-Instruments/uSEQ/releases",
-              type: "GET",
-              data: { "accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28" },
-              error: function (xhr, ajaxOptions, thrownError) {
-              }
-            }).then(function (data) {
-              const re = /uSEQ_(.*)_(([0-9])\.([0-9]))/g;
-              const matches = re.exec(data[0]['tag_name']);
-              const version = matches[2];
-              const ghVersionMajor = matches[3];
-              const ghVersionMinor = matches[4];
-              console.log(version);
-
-              //compare version
-              if (ghVersionMajor > moduleVersionMajor ||
-                (ghVersionMinor > moduleVersionMinor && ghVersionMajor >= moduleVersionMajor)) {
-                //new release available
-                post("There is a new firmware release available, click below to download");
-                post(`<a target="blank" href="${data[0]['html_url']}">${data[0]['html_url']}</a>`);
-                post("Information on how to update the module:");
-                post(`<a target="blank" href="https://emutelabinstruments.co.uk/useqinfo/useq-update/">https://emutelabinstruments.co.uk/useqinfo/useq-update/</a>`);
-              }
-
-
-            });
-
-          });
+          sendTouSEQ("@(useq-report-firmware-info)", upgradeCheck);
         }).catch((err) => {
           console.log(err);
           //connection failed
