8052633573350adab7103d5150df57f68ab1efb0
chriskiefer <chrisk13fer@gmail.com>
2024-08-09 20:56:52 +0100

    latest

--- src/openCam.mjs
+++ src/openCam.mjs
@@ -1,28 +1,36 @@
 export function openCam() {
-  let error = false;
+  // let error = false;
   let allMediaDevices = navigator.mediaDevices;
   if (!allMediaDevices || !allMediaDevices.getUserMedia) {
     console.log("getUserMedia() not supported.");
     return;
   }
-  allMediaDevices.getUserMedia({
-    audio: false,
-    video: { width: 1920, height: 1080 }
-  })
-    .then(function (vidStream) {
-      var video = document.getElementById('videopanel');
-      if ("srcObject" in video) {
-        video.srcObject = vidStream;
-      } else {
-        video.src = window.URL.createObjectURL(vidStream);
-      }
-      video.onloadedmetadata = function (e) {
-        video.play();
-      };
+  try {
+    allMediaDevices.getUserMedia({
+      audio: false,
+      video: { width: 1200, height: 800 }
     })
-    .catch(function (e) {
-      console.log(e.name + ": " + e.message);
-      error = true;
-    });
-  return error;
+      .then(function (vidStream) {
+        console.log(vidStream)
+        var video = document.getElementById('videopanel');
+        if ("srcObject" in video) {
+          video.srcObject = vidStream;
+        } else {
+          video.src = window.URL.createObjectURL(vidStream);
+        }
+        video.onloadedmetadata = function (e) {
+          video.play();
+        };
+      })
+      .catch(function (e) {
+        console.log(e.name + ": " + e.message);
+        error = true;
+      });
+  }
+   catch(err) {
+    console.log(err)
+  }
+  // return error;
+
+  return true;
 }
