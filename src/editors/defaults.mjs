7aa91f7d6009d1eda19ecb2ed32448e86af88fda
w1n5t0n <w1n5t0n@lnfinitemonkeys.org>
2025-03-21 18:54:51 +0000

    Add keywords and strings to theme testing default code

--- src/editors/defaults.mjs
+++ src/editors/defaults.mjs
@@ -3,15 +3,26 @@ export const defaultTheme = 'uSEQ Dark';
 
 // export const defaultEditorStartingCode = Array(100).fill("\n").join("");
 
-export const defaultThemeEditorStartingCode = 
+const defaultCode = 
 `(do
   ;; CV outs
   (a1 beat)
   (a2 (slow 2 beat))
-  (a3 (slow 3 beat))
+  (a3 (slow ([3 3 6 8 9 8 3] (slow 2 bar)) beat))
   ;; Gate outs
   (d1 (sqr beat))
   (d2 (sqr (fast 2 beat)))
   (d3 (sqr (fast 3 beat))))`;
 
-export const defaultMainEditorStartingCode = `${defaultThemeEditorStartingCode}${Array(91).fill('\n').join('')}`;
+export const defaultThemeEditorStartingCode = 
+`(do
+  ;; CV outs
+  (a1 beat) :keywords
+  (a2 (slow 2 beat)) "this is a string"
+  (a3 (slow ([3 3 6 8 9 8 3] (slow 2 bar)) beat))
+  ;; Gate outs
+  (d1 (sqr beat))
+  (d2 (sqr (fast 2 beat)))
+  (d3 (sqr (fast 3 beat))))`;
+
+export const defaultMainEditorStartingCode = `${defaultCode}${Array(91).fill('\n').join('')}`;
