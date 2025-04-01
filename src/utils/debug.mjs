bb032c993b23a9e621cbb90ae6fc433e808780d6
w1n5t0n <w1n5t0n@lnfinitemonkeys.org>
2025-04-01 15:17:40 +0100

    Make dbg take variable args

--- src/utils/debug.mjs
+++ src/utils/debug.mjs
@@ -2,9 +2,17 @@
 let debug = false;
 
 // Exported function to log messages if debug is enabled
-export function dbg(message) {
-  if (debug) {
-    console.log(message);
+export function dbg(...args) {
+  if (!debug) return;
+
+  if (args.length === 1) {
+    console.log(args[0]);
+  } else if (args.length === 2) {
+    console.log(`[${args[0]}]`, args[1]);
+  } else if (args.length === 3) {
+    console.log(`[${args[0]}][${args[1]}]`, args[2]);
+  } else {
+    console.log("[DEBUG]", ...args);
   }
 }
 
