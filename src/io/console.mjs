f6b3f5efcc336d23a6b32627235be8774d9c44d8
w1n5t0n <w1n5t0n@lnfinitemonkeys.org>
2025-04-03 10:46:30 +0100

    Make console promp'ts '>' use the accent colour and be bold

--- src/io/console.mjs
+++ src/io/console.mjs
@@ -9,7 +9,7 @@ const MAX_CONSOLE_LINES = 1000; // Increased from 50 to 1000 since we'll have sc
  * @param {string} value - Text to display (can include markdown)
  */
 export function post(value) {
-  consoleLines.push(marked.parse('`> ' + value + '`'));
+  consoleLines.push('<span style="color: var(--accent-color); font-weight: bold; display: inline;">> </span>' + marked.parse(value).replace(/^<p>|<\/p>$/g, ''));
   
   if (consoleLines.length > MAX_CONSOLE_LINES) {
     consoleLines.shift(); // Remove oldest line
