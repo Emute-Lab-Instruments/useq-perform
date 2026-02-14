39013db61bd67b8e0919d7af6f573d8002f539c2
w1n5t0n <w1n5t0n@lnfinitemonkeys.org>
2026-02-14 16:14:54 +0000

    build(vite): set relative base for static output

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  base: "./",
   plugins: [solid()],
   build: {
     target: "es2020",
